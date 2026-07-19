4538ea05c8f555e7caeed28a7ccc7b6acf8a282b
Devika Chhabra <chhabradevika796@gmail.com>
2026-07-19 11:38:22 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (21.71%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public void reverseString(char[] s) {
         int start = 0;
-        int end= s.length - 1;
+        int end = s.length - 1;
         while(start<end){
             char temp = s[start];
             s[start] = s[end];
@@ -9,5 +9,6 @@ class Solution {
             start++;
             end--;
         }
+        
     }
 }
