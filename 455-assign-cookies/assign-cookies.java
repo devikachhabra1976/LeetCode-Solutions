02901ec94a144811db9e6f295b2deffe88c41096
Devika Chhabra <chhabradevika796@gmail.com>
2026-07-27 20:59:31 +0530

    Time: 14 ms (92.15%) | Memory: 51.5 MB (55.82%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -6,13 +6,14 @@ class Solution {
         Arrays.sort(s);
         int left = 0;
         int right = 0;
-        while(left<m && right < n){
+        while(left < m && right < n){
             if(g[right]<=s[left]){
                 right++;
             }
             left++;
         }
 
-        return right;
+         return right;
     }
+   
 }
