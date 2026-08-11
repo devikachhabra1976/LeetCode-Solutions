22805859903a8a034d729637d64502c4cd44355d
Devika Chhabra <chhabradevika796@gmail.com>
2026-08-11 12:10:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (52.25%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int min = 0;
+        int max = 0;
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='('){
+                min++;
+                max++;
+            }
+            else if(c==')'){
+                min--;
+                max--;
+
+            }
+            else{
+                min--;
+                max++;
+            }
+
+            if(min<0){
+                min = 0;
+            }
+            if(max<0){
+                return false;
+            }
+
+            
+        }
+        return (min == 0);
+    }
+}
