026297a514bf07a9d58c3297cc2885155c7dd11c
Devika Chhabra <chhabradevika796@gmail.com>
2026-08-13 12:18:52 +0530

    Time: 6 ms (4.19%) | Memory: 48.1 MB (35.10%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -6,19 +6,17 @@ class Solution {
         for(int i=0;i<n;i++){
             while(!st.isEmpty() && height[i]>height[st.peek()]){
                 int bottom = st.pop();
-
                 if(st.isEmpty()){
                     break;
                 }
-                int left = st.peek();
 
+                int left = st.peek();
                 int width = i - left - 1;
                 int minHeight = Math.min(height[left],height[i]);
-                water+=width * (minHeight - height[bottom]);
+                water += width *(minHeight - height[bottom]);
 
             }
             st.push(i);
-
         }
         return water;
     }
