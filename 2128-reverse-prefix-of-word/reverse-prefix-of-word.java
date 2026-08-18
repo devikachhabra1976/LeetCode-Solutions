cba222b20da11fdf4f55cd0b5fbd90815152c7d8
Devika Chhabra <chhabradevika796@gmail.com>
2026-08-18 11:52:44 +0530

    Time: 1 ms (48.30%) | Memory: 42.8 MB (86.47%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int idx = word.indexOf(ch);
+        StringBuilder sb = new StringBuilder();
+        for(int i=idx;i>=0;i--){
+            sb.append(word.charAt(i));
+        }
+        for(int i=idx+1;i<word.length();i++){
+        sb.append(word.charAt(i));
+        }
+      return sb.toString();
+    }
+}
