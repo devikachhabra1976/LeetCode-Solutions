c98b6d53bd2f0b2b1965e7e8c0066cd8ccef1e1c
Devika Chhabra <chhabradevika796@gmail.com>
2026-09-09 12:15:27 +0530

    Time: 51 ms (46.70%) | Memory: 47.4 MB (62.58%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,27 +1,4 @@
 class Solution {
-
-    public boolean solve(int i,int k,int[] arr,int[][] dp){
-        if(k == 0) return true;
-
-        if(i==0) return arr[0] == k;
-
-        if(dp[i][k]!=-1){
-            return dp[i][k] == 1;
-        }
-
-        boolean nT = solve(i-1,k,arr,dp);
-
-        boolean take = false;
-        if(arr[i]<=k){
-            take = solve(i-1,k - arr[i],arr,dp);
-        }
-
-        boolean ans = take || nT;
-
-        dp[i][k] = ans ? 1: 0;
-
-        return ans;
-    }
     public boolean canPartition(int[] nums) {
         int n = nums.length;
         int sum = 0;
@@ -29,17 +6,33 @@ class Solution {
             sum += nums[i];
         }
 
-        if(sum % 2!=0){
+        if(sum %2!=0){
             return false;
         }
 
-        int target = sum / 2;
+        int k = sum / 2;
 
-        int[][] dp = new int[n][target + 1];
+        boolean[][] dp = new boolean[n][k+1];
         for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+            dp[i][0] = true;
         }
 
-        return solve(n-1,target,nums,dp);
+        if(nums[0]<=k){
+            dp[0][nums[0]] = true;
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=k;j++){
+                boolean nT = dp[i-1][j];
+
+                boolean take = false;
+                if(nums[i]<=j){
+                    take = dp[i-1][j - nums[i]];
+                }
+
+                dp[i][j] = take || nT;
+            }
+        }
+        return dp[n-1][k];
     }
 }
