83e3926c265e7a75572c8a022814617f406602b1
Devika Chhabra <chhabradevika796@gmail.com>
2026-09-21 11:37:48 +0530

    Time: 6 ms (73.58%) | Memory: 45.2 MB (38.76%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,47 +1,45 @@
 class Solution {
-
-    public int solve(int i,int k,int[] nums,int[][] dp){
-        if(i==0){
-            if(k==0 && nums[0] == 0){
-                return 2;
-            }
-            if(k==0 || nums[0] == k){
-                return 1;
-            }
-            return 0;
-        }
-
-        if(dp[i][k]!=-1) return dp[i][k];
-        int nt = solve(i-1,k,nums,dp);
-        int take = 0;
-        if(nums[i]<=k){
-            take = solve(i-1,k-nums[i],nums,dp);
-        }
-
-        dp[i][k] = take + nt;
-        return dp[i][k];
-    }
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
         int sum = 0;
         for(int i=0;i<n;i++){
             sum += nums[i];
+
+
         }
-        
 
         if(sum < Math.abs(target)){
             return 0;
+
         }
 
-        if((sum + target)%2!=0){
-            return 0;
-        }
+        if((sum + target)%2!=0) return 0;
         int k = (sum + target)/2;
+
         int[][] dp = new int[n][k+1];
         for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+            dp[i][0] = 1;
+        }
+        if(nums[0] == 0){
+            dp[0][0] = 2;
         }
 
-        return solve(n-1,k,nums,dp);
+        else if(nums[0]<=k){
+            dp[0][nums[0]] = 1;
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=k;j++){
+                int nT = dp[i-1][j];
+                int take = 0;
+                if(nums[i]<=j){
+                    take = dp[i-1][j-nums[i]];
+                }
+
+                dp[i][j] = take + nT;
+            }
+        }
+        return dp[n-1][k];
+
     }
 }
