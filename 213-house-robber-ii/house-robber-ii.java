d688df8eabd66f15026d93b8f75514e03d621550
Devika Chhabra <chhabradevika796@gmail.com>
2026-09-23 20:46:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (22.92%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,55 +1,31 @@
-import java.util.Arrays;
-
 class Solution {
-
+    public int solve(int i,int[] nums,int[] dp){
+        if(i<=0) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int left = nums[i-1]+solve(i-2,nums,dp);
+        int right = solve(i-1,nums,dp);
+        return dp[i] = Math.max(left,right);
+       
+    }
     public int rob(int[] nums) {
-
         int n = nums.length;
-
-        if(n == 1) {
-            return nums[0];
+        if(n==1) return nums[0];
+        
+        int[] arr1 = new int[n-1];
+        int[] arr2 = new int[n-1];
+        int[] dp1= new int[n];
+        int[] dp2 = new int[n];
+        Arrays.fill(dp1,-1);
+        Arrays.fill(dp2,-1);
+        for(int i=0;i<=n-2;i++){
+            arr1[i] = nums[i];
         }
-
-        // Case 1: exclude last house
-        int case1 = solveRange(nums, 0, n - 2);
-
-        // Case 2: exclude first house
-        int case2 = solveRange(nums, 1, n - 1);
-
-        return Math.max(case1, case2);
-    }
-
-    public int solveRange(int[] nums, int start, int end) {
-
-        int n = nums.length;
-
-        int[] dp = new int[n];
-
-        Arrays.fill(dp, -1);
-
-        return solve(end, start, nums, dp);
-    }
-
-    public int solve(int i, int start, int[] arr, int[] dp) {
-
-        if(i < start) {
-            return 0;
+        for(int i=1;i<n;i++){
+            arr2[i-1] =  nums[i];
         }
-
-        if(i == start) {
-            return arr[i];
-        }
-
-        if(dp[i] != -1) {
-            return dp[i];
-        }
-
-        int pick = arr[i] + solve(i - 2, start, arr, dp);
-
-        int nP = solve(i - 1, start, arr, dp);
-
-        dp[i] = Math.max(pick, nP);
-
-        return dp[i];
+        int res = solve(n-1,arr1,dp1);
+        int res2 = solve(n-1,arr2,dp2);
+        return Math.max(res,res2);
+        
     }
 }
