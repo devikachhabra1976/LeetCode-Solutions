a9f224f17385d12f8aa74556ade5b3b3f2c616ab
Devika Chhabra <chhabradevika796@gmail.com>
2026-09-23 20:51:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (22.92%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,12 +1,4 @@
 class Solution {
-    public int solve(int i,int[] nums,int[] dp){
-        if(i<=0) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int left = nums[i-1]+solve(i-2,nums,dp);
-        int right = solve(i-1,nums,dp);
-        return dp[i] = Math.max(left,right);
-       
-    }
     public int rob(int[] nums) {
         int n = nums.length;
         if(n==1) return nums[0];
@@ -15,17 +7,28 @@ class Solution {
         int[] arr2 = new int[n-1];
         int[] dp1= new int[n];
         int[] dp2 = new int[n];
-        Arrays.fill(dp1,-1);
-        Arrays.fill(dp2,-1);
+       
         for(int i=0;i<=n-2;i++){
             arr1[i] = nums[i];
         }
         for(int i=1;i<n;i++){
             arr2[i-1] =  nums[i];
         }
-        int res = solve(n-1,arr1,dp1);
-        int res2 = solve(n-1,arr2,dp2);
-        return Math.max(res,res2);
+        dp1[0] = 0;
+        dp1[1] = arr1[0];
+        for(int i=2;i<n;i++){
+         int left = arr1[i-1]+dp1[i-2];
+        int right = dp1[i-1];
+         dp1[i] = Math.max(left,right);
+        }
+           dp2[0] = 0;
+        dp2[1] = arr2[0];
+        for(int i=2;i<n;i++){
+         int left = arr2[i-1]+dp2[i-2];
+        int right = dp2[i-1];
+         dp2[i] = Math.max(left,right);
+        }
+        return Math.max(dp1[n-1],dp2[n-1]);
         
     }
 }
